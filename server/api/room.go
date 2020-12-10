baea2c7d8a15ab4ed4a2f11dab81cb7f6a3ed0ac
krypticallusion <krypticallusion@gmail.com>
2020-12-10 17:41:56 +0530

    🔒 (member): Only allow a base64 encode of member in endpoints, remove member generation
    Generate a member using /member/create
    Encode to base64 and feed these endpoints.
    They hungry!
    
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>

--- server/api/room.go
+++ server/api/room.go
@@ -2,6 +2,7 @@ package api
 
 import "C"
 import (
+	"encoding/base64"
 	"encoding/json"
 	"github.com/daemon1024/dokidoki/server/api/utils"
 	"github.com/daemon1024/dokidoki/server/entities"
@@ -10,16 +11,26 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	"github.com/google/uuid"
-	"log"
 )
 
 func CreateRoom(ctx *fiber.Ctx) error {
 	name := ctx.FormValue("name", "doki's room")
-	creator := ctx.FormValue("creator", "owner")
 
-	member, err := entities.GenerateMember(creator)
+	//Expecting a base64 encode of Member JSON
+	creator := ctx.FormValue("creator", "")
+
+	if creator == "" {
+		return fiber.NewError(fiber.StatusNotAcceptable, "FormKey creator not found")
+	}
+
+	cB, err := base64.StdEncoding.DecodeString(creator)
 	if err != nil {
-		log.Println(err)
+		return fiber.NewError(fiber.StatusNotAcceptable, "FormKey creator invalid")
+	}
+
+	var m entities.Member
+	if err := json.Unmarshal(cB, &m); err != nil {
+		return fiber.NewError(fiber.StatusNotAcceptable, "FormKey creator invalid")
 	}
 
 	id, err := uuid.NewRandom()
@@ -30,9 +41,9 @@ func CreateRoom(ctx *fiber.Ctx) error {
 	r := entities.Room{
 		ID:        id.String(),
 		Name:      name,
-		CreatedBy: member,
+		CreatedBy: m,
 		Status:    entities.PlayerStatus{},
-		Members:   []entities.Member{member},
+		Members:   []entities.Member{m},
 	}
 
 	if err := r.ToDb(); err != nil {
@@ -49,7 +60,24 @@ func CreateRoom(ctx *fiber.Ctx) error {
 
 func MyRoom(conn *websocket.Conn) {
 	id := conn.Params("id")
-	m := conn.Cookies("member")
+
+	//Expecting a memberJson base64 encoded
+	mCookies := conn.Cookies("member")
+
+	mB, err := base64.StdEncoding.DecodeString(mCookies)
+	if err != nil {
+		_ = conn.WriteMessage(websocket.CloseInternalServerErr, []byte("Cookie : member invalid"))
+		_ = conn.Close()
+		return
+	}
+
+	var m entities.Member
+	if err := json.Unmarshal(mB, &m); err != nil {
+		_ = conn.WriteMessage(websocket.CloseInternalServerErr, []byte("Cookie : member invalid"))
+		_ = conn.Close()
+		return
+	}
+
 
 	if !sider.IsRoomInDb(id) {
 		_ = conn.WriteMessage(websocket.CloseMessage, []byte("ROOM DOESN'T EXIST"))
@@ -57,15 +85,7 @@ func MyRoom(conn *websocket.Conn) {
 		return
 	}
 
-	member, err  := entities.GenerateMember(m)
-	if err != nil {
-		//I have no idea how to properly send a websocket close result. I'm Pardoning myself here - k
-		_ = conn.WriteMessage(websocket.CloseMessage, []byte(err.Error()))
-		_ = conn.Close()
-		return
-	}
-
-	r, err := utils.AddMemberToRoom(id, member)
+	r, err := utils.AddMemberToRoom(id, m)
 	if err != nil {
 		_ = conn.WriteMessage(websocket.CloseMessage, []byte(err.Error()))
 		_ = conn.Close()
@@ -85,7 +105,7 @@ func MyRoom(conn *websocket.Conn) {
 
 	for {
 		if _, _, err := conn.ReadMessage(); err != nil {
-			r, err = utils.RemoveMemberFromRoom(id, member.ID)
+			r, err = utils.RemoveMemberFromRoom(id, m.ID)
 			if err != nil {
 				_ = conn.Close()
 				return
